6e2abc53de36f0c9529e9916d2f6bed90ed82536
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-02-27 21:53:44 +0600

    adding and multiplying problem of complex number has been solved with oop

--- complex-number-with-oop.cpp
+++ complex-number-with-oop.cpp
@@ -10,14 +10,20 @@ class Complex{
         int imaginary;
     
     public:
-    
+
         Complex(int real, int imaginary){
             this -> real=real;
             this -> imaginary=imaginary;
         }
         
         void print(){
-            cout << real<< "+i" << imaginary << endl;
+            if(imaginary>0){
+                cout<< real<< "+i" << imaginary << endl;
+            }else if(imaginary<0){
+                cout<< real<< "-i" <<((-1)* imaginary) << endl;
+            }else{
+                cout<<real<<endl;
+            }
         }
         
         // void simplify(){
@@ -32,10 +38,14 @@ class Complex{
         //     denominator=denominator/gcd;
         // }
         
-        // void prod(Complex f2){
-        //     real=real+f2.real-imaginary*f2.imaginary;
-        //     imaginary=real*f2.imaginary+f2.real*imaginary;
-        // }
+        void prod(Complex f2){
+            int x1=real;
+            int x2=f2.real;
+            int y1=imaginary;
+            int y2=f2.imaginary;
+            real=x1*x2-y1*y2;
+            imaginary=x1*y2+x2*y1;
+        }
         void add(Complex f2){
             real=real+f2.real;
             imaginary=imaginary+f2.imaginary;
@@ -45,12 +55,20 @@ class Complex{
 
 
 int main() {
-    Complex f1(10,2);
-    Complex f2(15,4);
+    Complex f1(3,4);
+    Complex f2(3,-4);
+    int choice;
     
-    f1.add(f2);
-    // f1.prod(f2);
-    f1.print();
+    cin>>choice;
+    if(choice==0){
+        f1.prod(f2);
+        f1.print();
+    }else {
+        f1.add(f2);
+        f1.print();
+        
+    }
+    
 
     return 0;
 }
