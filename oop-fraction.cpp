89bff84c106b0382564616ca2aed10cd1133cfdf
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-02-27 16:07:08 +0600

    fraction adding method solved using oop

--- oop-fraction.cpp
+++ oop-fraction.cpp
@@ -20,12 +20,37 @@ class Fraction{
             cout << numerator<< "/" << denominator << endl;
         }
         
+        void simplify(){
+            int gcd=1;
+            int j=min(numerator,denominator);
+            for(int i=1;i<=j;i++){
+                if(numerator%i==0 && denominator%i==0){
+                    gcd=i;
+                }
+            }
+            numerator=numerator/gcd;
+            denominator=denominator/gcd;
+        }
         
-}
+        void add(Fraction f2){
+            int lcm =denominator*f2.denominator;
+            int x=lcm/denominator;
+            int y=lcm/f2.denominator;
+            int num=(numerator*x)+(f2.numerator*y);
+            numerator=num;
+            denominator=lcm;
+            simplify();
+        }
+        
+};
 
 
 int main() {
-    cout << "Hello world!";
+    Fraction f1(10,2);
+    Fraction f2(15,4);
+    
+    f1.add(f2);
+    f1.print();
 
     return 0;
 }
